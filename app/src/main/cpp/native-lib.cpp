75e0899961f94d4ca3ac938b93a6c11a48fdecc2
Simonla <zengmingyang96@gmail.com>
2019-03-23 22:27:44 +0800

    Integrated FFmpeg

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,14 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include "libavcodec/avcodec.h"
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_mingyangzeng_ffmpgedemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
 }
